79ef43eb0ac6ee1b26f31141232be546e9af4ecb
DItzzCode <radityahuuu@gmail.com>
2024-12-08 14:16:19 +0800

    mengelola api

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 
 
 
-(async () => {
+async function* getApi() {
   const api = await fetch("https://reqres.in/api/users");
   const result = await api.json();
   const { data } = result;
 
-  data.forEach(({ email }) => {
-    console.log("email : " , email)
-  })
+const email = data.forEach(({email , id}) => {
+  console.log("id :" , id)
+  console.log("email :" , email);
+  console.log("")
+})
+
+yield email;
+}
+
+(async () => {
+const takeAPI = getApi();
+console.log( await takeAPI.next());
 })();
+
