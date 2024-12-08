d606a37bd577c4c96385df93e38180205f195287
DItzzCode <radityahuuu@gmail.com>
2024-12-08 13:53:30 +0800

    membuat async function

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-console.log("hello world");
 
-const nama = "Radti";
-console.log(nama)
+
+
+(async () => {
+  const api = await fetch("https://reqres.in/api/users");
+  const result = await api.json();
+  const { data } = result;
+
+  data.forEach(({ email }) => {
+    console.log("email : " , email)
+  })
+})();
